84980a3def6ac8e9033adff6b2a5876327d2b5d3
rafael.gruhn <rafa.gruhn@gmail.com>
2019-06-28 01:14:56 -0300

    Alterações pequenas

--- livros/admin.py
+++ livros/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from livros.models import Autor, Livro
 
-# Register your models here.
+class AdminLivro(admin.ModelAdmin):
+    model = Livro
+    list_display = ('titulo', 'editora', 'data', 'status')
+    list_filter = ('editora',)
 
 admin.site.register(Autor)
-admin.site.register(Livro)
+admin.site.register(Livro, AdminLivro)
--- livros/models.py
+++ livros/models.py
@@ -12,7 +12,8 @@ CHOICES_LIVRO_STATUS = (
 
 
 class Autor(models.Model):
-    nome = models.CharField(max_length = 100, blank = False, null = False)
+    nome = models.CharField(max_length = 100, blank = False,
+    null = False)
     email = models.EmailField()
     class Meta:
         verbose_name_plural = "Autores"
@@ -21,15 +22,15 @@ class Autor(models.Model):
 
 
 class Livro(models.Model):
-    titulo = models.CharField(max_length = 150, blank = False, null = False)
+    titulo = models.CharField(max_length = 150, blank = False,
+    null = False)
     editora = models.CharField(max_length = 100)
-    data = models.DateField(null = False, default=datetime.date.today)
-    status = models.BooleanField("Retirado: ", choices = CHOICES_LIVRO_STATUS)
-    autor = models.ForeignKey(Autor, related_name = 'livros',on_delete = models.PROTECT)
-    user = models.ForeignKey(
-        User, related_name = 'livros_retirados',
-        verbose_name="Usuario", blank = True, null = True,
-        on_delete=models.PROTECT)
+    data = models.DateField(null = False,
+    default=datetime.date.today)
+    status = models.BooleanField("Retirado: ",
+    choices = CHOICES_LIVRO_STATUS)
+    autor = models.ForeignKey(Autor, related_name = 'livros',
+    on_delete = models.CASCADE)
 
     def __str__(self):
         return self.titulo
